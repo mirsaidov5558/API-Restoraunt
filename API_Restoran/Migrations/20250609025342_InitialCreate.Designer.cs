077de047d166455855de8d0db011b66e5963b8a1
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-06-09 07:56:52 +0500

    DB

--- API_Restoran/Migrations/20250609025342_InitialCreate.Designer.cs
+++ API_Restoran/Migrations/20250609025342_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace API_Restoran.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250608183052_InitialCreate")]
+    [Migration("20250609025342_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
